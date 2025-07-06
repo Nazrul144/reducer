b90880b69e7b45eb79de4a2bfcbc7aae6f3778ab
Nazrul Islam <nazrulislam.cse28@gmail.com>
2025-07-06 06:39:49 +0600

    porject polish

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,26 @@ import { decrement, increment } from "./redux/features/counters/counterSlice";
 
 
 function App() {
-  const counter = useSelector((store)=> store.value);
+  const counter = useSelector((store) => store.counter);
+  console.log(counter)
   const dispatch = useDispatch()
 
-  const handleIncrement = ()=>{
+  const handleIncrement = () => {
     dispatch(increment())
   }
-  const handleDecrement = ()=>{
+  const handleDecrement = () => {
     dispatch(decrement())
   }
 
   return (
     <>
-    <div>
-      <p>{counter}</p>
-      <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
-    </div>
+      <div>
+        <p>{counter}</p>
+        <div className="flex gap-3 mt-4">
+          <button className="font-bold border-2 border-amber-800  p-2 rounded-xl cursor-pointer" onClick={handleIncrement}>Increment</button>
+          <button className="font-bold border-2 border-amber-800  p-2 rounded-xl cursor-pointer" onClick={handleDecrement}>Decrement</button>
+        </div>
+      </div>
     </>
   )
 }
