74341c060f57f55e74b67a257fac547c1a060886
Nazrul Islam <nazrulislam.cse28@gmail.com>
2025-07-06 06:23:51 +0600

    Slice is created in reducers

---
+++ src/redux/features/counters/counterSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface CounterState {
+    value: number
+}
+
+const initialState: CounterState = {
+    value: 0
+}
+
+const counterSlice = createSlice({
+    name: "counters",
+    initialState,
+    reducers: {
+        increment: (state) => {
+            state.value++
+        },
+        decrement: (state) => {
+            state.value++
+        }
+    }
+})
+
+export default counterSlice.reducer;
+
+export const {increment, decrement} = counterSlice.actions;
